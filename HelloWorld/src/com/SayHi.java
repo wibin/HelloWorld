84b644833e66204d74e59836f2bb24b45d074fc7
wzb <wzb@wzb-PC>
2014-10-11 13:20:23 +0800

    说你也爱我

--- HelloWorld/src/com/SayHi.java
+++ HelloWorld/src/com/SayHi.java
@@ -5,6 +5,9 @@ public class SayHi {
 	public void sayHi(String s) {
 		// ËµÎÒ°®Äã
 		System.out.println("Hi, I love you, " + s + "!");
+		
+		// you say
+		System.out.println("Say 'you love me, too!");
 	}
 	
 }
